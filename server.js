a5522f1538ab4d5ad4ba7b9fc0c6bb6ea9467af2
oraclespainpresales <oraclespainpresales@gmail.com>
2019-04-21 01:12:14 +0200

    fix

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const log = require('npmlog-ts')
     , util = require('util')
     , express = require('express')
     , bodyParser = require('body-parser')
+    , glob = require("glob")
     , fs = require('fs-extra')
 ;
 
 const PROCESS   = "PROCESS"
     , WWWFOLDER = "/var/www/mapviewer/"
     , PORT      = 9007
-    , URI       = "/wedoindustry/truck/route/:demozone"
+    , URI       = "/wedoindustry/truck/route/:demozone/:uuid"
 ;
 
 log.timestamp = true;
@@ -29,14 +30,21 @@ app.use(bodyParser.json());
 **/
 
 app.post(URI, (req, res) => {
-    let demozone = req.params.demozone;
-    if (!demozone || !req.body || !req.body.payload) {
-      res.status(400).send().end();
-      return;
-    }
-    let file = WWWFOLDER + demozone.toLowerCase() + '.js';
-    fs.writeFileSync(file, "var route = " + req.body.payload);
+  let demozone = req.params.demozone;
+  let uuid = req.params.uuid;
+  if (!demozone || !uuid || !req.body || !req.body.payload) {
+    res.status(400).send().end();
+    return;
+  }
+  demozone = demozone.toLowerCase();
+  glob( demozone + '*.js', (er, files) => {
+    _.forEach(files, (f) => {
+      fs.removeSync(f);
+    });
+    let file = WWWFOLDER + demozone + '.' + uuid + '.js';
+    fs.writeFileSync(file, "var route = " + req.body.payload + ";");
     res.status(204).send().end();
+  });
 });
 
 //////////////////////////////////////////////////////////
